4ce1f42015a48668a63bb852cc666a9eed25b454
adridjs <adjsanchez-@hotmail.com>
2020-03-13 19:57:04 +0100

    added professions.txt for measuring direct bias

--- word2vec/utils/constants.py
+++ word2vec/utils/constants.py
@@ -7,3 +7,4 @@ LANGUAGES = {'en', 'es'}
 # Choose any model you want for each language, these are from spacy and only used to clean the sentence from useless characters
 # and normalizing it  (verb tenses, honorifics, stop words) in order to have a semantically sparse word vector space
 NLP_MODELS = {'en': 'en_core_web_sm', 'es': 'es_core_news_sm'}
+
