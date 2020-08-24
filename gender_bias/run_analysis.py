f4845044a79e3fbd2a31ac3b2c003dac83228f5e
adridjs <adjsanchez-@hotmail.com>
2020-08-24 20:59:14 +0200

    Default values for PCA.

--- gender_bias/run_analysis.py
+++ gender_bias/run_analysis.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     dd = DataDriver(corpus_folder=corpus_folder, languages={'en'})
     for corpus in ['balanced', 'EuroParl']:
         analysis = Analysis(language, corpus=corpus)
-        analysis.plot_pca(sample=10)
+        analysis.plot_pca()
